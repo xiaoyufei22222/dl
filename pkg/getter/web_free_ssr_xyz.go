6fdcee6a0395f11fbe64303edf12e5c848d7ffda
xiaoyufei22222 <94793967+xiaoyufei22222@users.noreply.github.com>
2021-11-24 23:42:38 +0800

    Update web_free_ssr_xyz.go

--- pkg/getter/web_free_ssr_xyz.go
+++ pkg/getter/web_free_ssr_xyz.go
@@ -2,12 +2,12 @@ package getter
 
 import (
 	"encoding/json"
-	"github.com/Sansui233/proxypool/log"
+	"github.com/xiaoyufei22222/proxypool-luo/log"
 	"io/ioutil"
 	"sync"
 
-	"github.com/Sansui233/proxypool/pkg/proxy"
-	"github.com/Sansui233/proxypool/pkg/tool"
+	"github.com/xiaoyufei22222/proxypool-luo/pkg/proxy"
+	"github.com/xiaoyufei22222/proxypool-luo/pkg/tool"
 )
 
 func init() {
