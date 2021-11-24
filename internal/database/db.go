19f8d95f077e6c65797d44d56297d057ada6c7ee
xiaoyufei22222 <94793967+xiaoyufei22222@users.noreply.github.com>
2021-11-24 23:08:36 +0800

    Update db.go

--- internal/database/db.go
+++ internal/database/db.go
@@ -1,10 +1,10 @@
 package database
 
 import (
-	"github.com/Sansui233/proxypool/log"
+	"github.com/xiaoyufei22222/proxypool/log"
 	"os"
 
-	"github.com/Sansui233/proxypool/config"
+	"github.com/xiaoyufei22222/proxypool/config"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
