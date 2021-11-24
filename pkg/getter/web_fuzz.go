4deeb42041f2cee7f6f5c1a41d58690c8818f03d
xiaoyufei22222 <94793967+xiaoyufei22222@users.noreply.github.com>
2021-11-24 23:43:10 +0800

    Update web_fuzz.go

--- pkg/getter/web_fuzz.go
+++ pkg/getter/web_fuzz.go
@@ -1,12 +1,12 @@
 package getter
 
 import (
-	"github.com/Sansui233/proxypool/log"
+	"github.com/xiaoyufei22222/proxypool-luo/log"
 	"io/ioutil"
 	"sync"
 
-	"github.com/Sansui233/proxypool/pkg/proxy"
-	"github.com/Sansui233/proxypool/pkg/tool"
+	"github.com/xiaoyufei22222/proxypool-luo/pkg/proxy"
+	"github.com/xiaoyufei22222/proxypool-luo/pkg/tool"
 )
 
 // Add key value pair to creatorMap(string → creator) in base.go
