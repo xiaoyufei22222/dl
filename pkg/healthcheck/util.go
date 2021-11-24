f9f8f245b30759802ad8bacbdbf22f99b9417411
xiaoyufei22222 <94793967+xiaoyufei22222@users.noreply.github.com>
2021-11-25 00:21:36 +0800

    Update util.go

--- pkg/healthcheck/util.go
+++ pkg/healthcheck/util.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"fmt"
 	C "github.com/Dreamacro/clash/constant"
-	"github.com/xiaoyufei22222/colly/tree/v2.1.0/proxy"
+	"github.com/xiaoyufei22222/colly/tree/v1.2.0/proxy"
 	"io/ioutil"
 	"net"
 	"net/http"
