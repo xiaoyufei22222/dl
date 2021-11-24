68872e34d8e58311befdab8fa90ef294faebbcbb
xiaoyufei22222 <94793967+xiaoyufei22222@users.noreply.github.com>
2021-11-24 23:23:44 +0800

    Update subscribe.go

--- pkg/getter/subscribe.go
+++ pkg/getter/subscribe.go
@@ -1,13 +1,13 @@
 package getter
 
 import (
-	"github.com/Sansui233/proxypool/log"
+	"github.com/xiaoyufei22222/proxypool-luo/log"
 	"io/ioutil"
 	"strings"
 	"sync"
 
-	"github.com/Sansui233/proxypool/pkg/proxy"
-	"github.com/Sansui233/proxypool/pkg/tool"
+	"github.com/xiaoyufei22222/proxypool-luo/pkg/proxy"
+	"github.com/xiaoyufei22222/proxypool-luo/pkg/tool"
 )
 
 // Add key value pair to creatorMap(string → creator) in base.go
