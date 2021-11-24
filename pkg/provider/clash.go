1d2cce6e0f79718424f92fdbb419c93051474756
xiaoyufei22222 <94793967+xiaoyufei22222@users.noreply.github.com>
2021-11-24 23:45:36 +0800

    Update clash.go

--- pkg/provider/clash.go
+++ pkg/provider/clash.go
@@ -1,10 +1,10 @@
 package provider
 
 import (
-	"github.com/Sansui233/proxypool/pkg/tool"
+	"github.com/xiaoyufei22222/proxypool-luo/pkg/tool"
 	"strings"
 
-	"github.com/Sansui233/proxypool/pkg/proxy"
+	"github.com/xiaoyufei22222/proxypool-luo/pkg/proxy"
 )
 
 // Clash provides functions that make proxies support clash client
