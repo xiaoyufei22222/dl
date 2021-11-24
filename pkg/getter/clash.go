a60612db0d7e90465686380af4714f66952ae937
xiaoyufei22222 <94793967+xiaoyufei22222@users.noreply.github.com>
2021-11-24 23:21:44 +0800

    Update clash.go

--- pkg/getter/clash.go
+++ pkg/getter/clash.go
@@ -1,9 +1,9 @@
 package getter
 
 import (
-	"github.com/Sansui233/proxypool/log"
-	"github.com/Sansui233/proxypool/pkg/proxy"
-	"github.com/Sansui233/proxypool/pkg/tool"
+	"github.com/xiaoyufei22222/proxypool/log"
+	"github.com/xiaoyufei22222/proxypool/pkg/proxy"
+	"github.com/xiaoyufei22222/proxypool/pkg/tool"
 	"gopkg.in/yaml.v3"
 	"io/ioutil"
 	"sync"
